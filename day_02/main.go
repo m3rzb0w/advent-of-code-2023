b97852bf0f4a4047aa8d853e7f5b6abd0fc605af
m3rzb0w <80rN70c0D3@protonmail.com>
2023-12-01 17:01:43 +0000

    prep d2

---
+++ day_02/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+// var url string = "https://adventofcode.com/2023/day/2/input"
+// var fileName = fetch.Filename(url)
+// var fileExists = fetch.CheckFileExists(fileName)
+// var session = fetch.Grabsession()
+// var input = fetch.Getfile(fileExists, fileName, url, session)
+
+func main() {
+	fmt.Println("hey mum")
+}

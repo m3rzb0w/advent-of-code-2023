85227094fbb81c9155c060440fe34fa29fecc75c
m3rzb0w <80rN70c0D3@protonmail.com>
2023-12-02 06:13:50 +0000

    d2 part1 dirty

--- day_02/main.go
+++ day_02/main.go
@@ -2,14 +2,73 @@ package main
 
 import (
 	"fmt"
+	fetch "getdata"
+	"log"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
-// var url string = "https://adventofcode.com/2023/day/2/input"
-// var fileName = fetch.Filename(url)
-// var fileExists = fetch.CheckFileExists(fileName)
-// var session = fetch.Grabsession()
-// var input = fetch.Getfile(fileExists, fileName, url, session)
+var url string = "https://adventofcode.com/2023/day/2/input"
+var fileName = fetch.Filename(url)
+var fileExists = fetch.CheckFileExists(fileName)
+var session = fetch.Grabsession()
+var input = fetch.Getfile(fileExists, fileName, url, session)
+
+//12 red cubes, 13 green cubes, and 14 blue cubes
+
+type Game struct {
+	ID    int
+	Red   int
+	Green int
+	Blue  int
+	Set   int
+}
+
+const maxRedCubesPartOne int = 12
+const maxGreenCubesPartOne int = 13
+const maxBlueCubesPartOne int = 14
+
+var countPartOne int
 
 func main() {
-	fmt.Println("hey mum")
+	input = strings.TrimSpace(input)
+	data := strings.Split(input, "\n")
+	for _, v := range data {
+		var game Game
+		currentGame := strings.Split(v, ";")
+		re := regexp.MustCompile(`(?:Game\s)(\d+)`)
+		reRed := regexp.MustCompile(`(\d+)(?:\sred)`)
+		reGreen := regexp.MustCompile(`(\d+)(?:\sgreen)`)
+		reBlue := regexp.MustCompile(`(\d+)(?:\sblue)`)
+		gameId, err := strconv.Atoi(re.FindStringSubmatch(currentGame[0])[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		game.ID = gameId
+		for _, set := range currentGame {
+			var currentRed int
+			var currentGreen int
+			var currentBlue int
+			if reRed.MatchString(set) {
+				currentRed, _ = strconv.Atoi(reRed.FindStringSubmatch(set)[1])
+			}
+			if reGreen.MatchString(set) {
+				currentGreen, _ = strconv.Atoi(reGreen.FindStringSubmatch(set)[1])
+			}
+			if reBlue.MatchString(set) {
+				currentBlue, _ = strconv.Atoi(reBlue.FindStringSubmatch(set)[1])
+			}
+
+			if currentRed > maxRedCubesPartOne || currentGreen > maxGreenCubesPartOne || currentBlue > maxBlueCubesPartOne {
+				fmt.Println("Game is KO", game.ID)
+				game.ID = 0
+
+			}
+
+			fmt.Println(currentRed, currentGreen, currentBlue)
+		}
+		countPartOne += game.ID
+		fmt.Println("Part one => ", countPartOne)
+	}
 }

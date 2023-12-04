154ac1a64a1cfe8d427a6d71c049428443bacf05
m3rzb0w <80rN70c0D3@protonmail.com>
2023-12-04 07:57:15 +0000

    dirty p2

--- day_04/main.go
+++ day_04/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	fetch "getdata"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -13,19 +14,40 @@ var fileExists = fetch.CheckFileExists(fileName)
 var session = fetch.Grabsession()
 var input = fetch.Getfile(fileExists, fileName, url, session)
 
+type Card struct {
+	ID         int
+	WinningNum []string
+	PlayerNum  []string
+}
+
+type CardSlice []Card
+
 func main() {
 	input = strings.TrimSpace(input)
 	data := strings.Split(input, "\n")
 
 	global := 0
 
+	var allCards CardSlice
+
+	matchingNumber := make(map[int]int)
+
 	for _, v := range data {
+		var card Card
 		lineSplit := strings.Split(v, "|")
 		regCard := regexp.MustCompile(`(?:Card\s+)(\d+)`)
 		cardId := regCard.FindStringSubmatch(lineSplit[0])[1]
 		cardWinningNums := strings.Split(strings.Split(lineSplit[0], ":")[1], " ")
 		playerNums := strings.Split(lineSplit[1], " ")
 		fmt.Println(cardId, cardWinningNums, playerNums)
+
+		cardIdNum, _ := strconv.Atoi(cardId)
+
+		card.ID = cardIdNum
+		card.WinningNum = cardWinningNums
+		card.PlayerNum = playerNums
+		allCards = append(allCards, card)
+
 		count := []string{}
 		tmpcount := 0
 		check := make(map[string]bool)
@@ -38,6 +60,11 @@ func main() {
 				}
 			}
 		}
+		matchingNumber[cardIdNum]++
+		for i := range count {
+			matchingNumber[cardIdNum+i+1]++
+		}
+
 		fmt.Println("count=>", count)
 		if len(count) == 1 {
 			tmpcount++
@@ -62,4 +89,38 @@ func main() {
 		check = make(map[string]bool)
 	}
 	fmt.Println("part1 =>", global)
+	fmt.Println(allCards)
+	fmt.Println(matchingNumber)
+	for _, card := range allCards {
+		if matchingNumber[card.ID] != 0 && matchingNumber[card.ID] != 1 {
+			loop := 1
+			for loop < matchingNumber[card.ID] {
+
+				count := []string{}
+				check := make(map[string]bool)
+				for _, v := range card.WinningNum {
+					for _, n := range card.PlayerNum {
+						if len(v) != 0 && len(n) != 0 && v == n && check[v] != true {
+							// fmt.Println("we have a match, ", v, n)
+							check[v] = true
+							count = append(count, v)
+						}
+					}
+				}
+
+				for i := range count {
+					matchingNumber[card.ID+i+1]++
+				}
+
+				loop++
+			}
+		}
+	}
+	fmt.Println(matchingNumber)
+	ans := 0
+	for _, val := range matchingNumber {
+		ans += val
+
+	}
+	fmt.Println(ans)
 }
